ebad383aa4cfa0c088559f02bdf1959f2a1c81c8
TheDrakl <denysmelnyk262626@gmail.com>
2024-12-24 17:27:05 +0000

    Created basic pricing plan

---
+++ frontend/app/components/features/PricingPlan.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="w-[65%] h-[40vh] min-h-full bg-transparent border-yellow-500 border-[2px] rounded-3xl font-roboto text-center"
+  >
+    <h1 class="text-2xl text-center pt-2 font-[700]">{{ title }}</h1>
+    <p class="text-sm font-[400] font-montserrat">${{ price }} / month</p>
+
+    <div>
+      <li class="list-none text-start p-2 items-center">
+        <ul v-for="feature in features" :key="feature" class="flex flex-row items-center mx-4 mb-4">
+            <Icon name="material-symbols:verified" class="mr-2 bg-gray-600"></Icon>
+          {{
+            feature
+          }}
+        </ul>
+
+      </li>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  features: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
--- frontend/app/components/sections/PricingPlansSection.vue
+++ frontend/app/components/sections/PricingPlansSection.vue
@@ -1,3 +1,20 @@
 <template>
-    <section class="mt-12 min-h-[120vh] lg:min-h-[90vh] bg-milkWhite"></section>
+  <section class="mt-12 min-h-[120vh] lg:min-h-[90vh] bg-milkWhite">
+    <div class="container mx-auto py-6">
+      <h1
+        class="font-[500] text-5xl lg:text-5xl text lg:leading-[1] pt-4 font-montserrat text-center"
+        data-aos="fade-center"
+        data-aos-offset="200"
+        data-aos-easing="ease-in-sine"
+      >
+        Pricing Plans
+      </h1>
+      <div class="grid grid-cols-4 mt-12 place-items-center">
+        <PricingPlan :title="'Basic Plan'" :price="20" :features="['feature1', 'feature2', 'feature2', 'feature2', 'feature2']"></PricingPlan>
+        <PricingPlan :title="'Basic Plan'" :price="20" :features="['feature1', 'feature2', 'feature2', 'feature2', 'feature2']"></PricingPlan>
+        <PricingPlan :title="'Basic Plan'" :price="20" :features="['feature1', 'feature2', 'feature2', 'feature2', 'feature2']"></PricingPlan>
+        <PricingPlan :title="'Basic Plan'" :price="20" :features="['feature1', 'feature2', 'feature2', 'feature2', 'feature2']"></PricingPlan>
+      </div>
+    </div>
+  </section>
 </template>
